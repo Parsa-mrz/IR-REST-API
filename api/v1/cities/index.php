5962daeb4428d2884cfa607ccb49305d1d0e9bc6
Parsa Mirzaie <Mirzaie_parsa@protonmail.ch>
2023-02-21 00:24:31 +0330

    fix bug for get param

--- api/v1/cities/index.php
+++ api/v1/cities/index.php
@@ -4,6 +4,34 @@ include_once("../../../loader.php");
 use App\Services\CityService;
 use App\Utilities\Response;
 
-$cs = new CityService();
-$result = $cs->getCities([1,2,3,4]);
-echo Response::respond($result,Response::HTTP_OK);
+$request_method = $_SERVER['REQUEST_METHOD'];
+
+$request_body = json_decode(file_get_contents('php://input'),true);
+
+switch($request_method){
+    case 'GET':
+        $city_service = new CityService();
+        $province_id = $_GET['province_id']?? null;
+        $request_data = [
+            'province_id' => intval($province_id)
+        ];
+        $response = $city_service->getCities($request_data);
+        Response::responseAndDie($response,Response::HTTP_OK);
+
+        break; 
+    case 'POST':
+        Response::responseAndDie('POST Request Method',Response::HTTP_OK);
+        
+        break;
+    case 'PUT':
+        Response::responseAndDie('PUT Request Method',Response::HTTP_OK);
+
+        break;
+    case 'DELETE':
+        Response::responseAndDie('DELETE Request Method',Response::HTTP_OK);
+        
+        break;
+    
+    default:
+    Response::responseAndDie('Invalid Request Method',Response::HTTP_METHOD_NOT_ALLOWED);
+}
