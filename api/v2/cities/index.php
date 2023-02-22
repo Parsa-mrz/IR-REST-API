c9e47f2540d4db0a1eda010a7d150f8d3673211a
Parsa Mirzaie <Mirzaie_parsa@protonmail.ch>
2023-02-22 03:48:49 +0330

    Add ORDER BY filter for GET response

--- api/v2/cities/index.php
+++ api/v2/cities/index.php
@@ -10,7 +10,7 @@ $request_method = $_SERVER['REQUEST_METHOD'];
 $request_body = json_decode(file_get_contents('php://input'),true);
 $city_service = new CityService();
 
-switch($request_method){
+switch($request_method){ 
     case 'GET':
         $province_id = $_GET['province_id']?? null;
         $request_data = [
